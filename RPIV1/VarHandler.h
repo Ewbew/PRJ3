96f6c35f7be2c49e348a99c2bb351ce35de3b3ca
Kmauxx <201706059@post.au.dk>
2025-05-19 15:40:45 +0200

    Doxygen update for VarHandler

--- RPIV1/VarHandler.h
+++ RPIV1/VarHandler.h
@@ -1,14 +1,41 @@
 #pragma once
 #include <string>
 
+/**
+ * @brief Handles variables and state for Bluetooth communication.
+ * 
+ * This class manages drive and shoot modes, message formatting,
+ * obstruction state, and socket connection status for the Bluetooth thread.
+ */
 class VarHandler {
 public:
+/**
+ * @brief Constructs a VarHandler with default values.
+ */
     VarHandler();
 
     // Drive mode
+    /**
+     * @brief Set the drive mode variable for the right wheel. 
+     * @param newValue The new value for drive mode 1.
+     */
     void setVar1DriveMode(const int& newValue);
+
+    /**
+     * @brief Get the drive mode variable for the right wheel.
+     * @return The value of drive mode 1. 
+     */
     int getVar1DriveMode() const;
+
+    /**
+     * @brief Set the drive mode variable for the left wheel. 
+     * @param newValue The new value for drive mode 2.
+     */
     void setVar2DriveMode(const int& newValue);
+    /**
+     * @brief Get the drive mode variable for the left wheel.
+     * @return The value of drive mode 2. 
+     */
     int getVar2DriveMode() const;
 
     // Shoot mode
