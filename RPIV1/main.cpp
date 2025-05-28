703f0368791c9ac967d89ea08f08f69cc04fdd7b
Otto Sejrskild Santesson <santessonen@gmail.com>
2025-05-28 11:10:07 +0200

    Deleted old, commented out code in the main

--- RPIV1/main.cpp
+++ RPIV1/main.cpp
@@ -13,40 +13,7 @@
 #include <QVBoxLayout>
 #include <QString>
 
-
-
 using namespace std;
-
-/* Old code; removed while debugging
-// Global stop signal
-atomic<bool> keepRunning(true);
-
-int main(int argc, char *argv[]) {
-    VarHandler handler;
-    string targetBluetoothAddress = "98:D3:51:FE:6F:30"; // REPLACE WITH MAC ADRESS OF PSOC
-    //THIS IS SUPER IMPORTANT OR IT WILL NOT WORK
-
-    // Start Bluetooth sender in its own thread
-    thread btThread(bluetoothSenderLoop, targetBluetoothAddress, &handler);
-
-    QApplication app(argc, argv);
-
-    SliderWindow window(&handler);
-    window.resize(800, 600);
-    window.show();
-
-    
-
-    // Stop the Bluetooth loop
-    keepRunning = false;
-    btThread.join();
-
-    cout << "Program complete." << endl;
-    return app.exec();
-}
-
-*/
-
 
 atomic<bool> keepRunning(true);
 
